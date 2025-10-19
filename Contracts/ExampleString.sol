1a6cd03b3a4a88a21a3c59b78fa932184be4c7b7
kartik <kaxxtric001@gmail.com>
2025-10-19 12:47:36 +0530

    Strings vs Bytes in Solidity

---
+++ Contracts/ExampleString.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+contract ExampleStrings {
+    string public myString="HEllo World";
+
+    bytes public myBytes="HEllo World";
+
+    function setMyString(string memory _myString) public {
+        myString = _myString;
+    }
+
+    function compareTwoStrings(string memory _myString) public view returns (bool) {
+        return keccak256(abi.encodePacked(myString)) == keccak256(abi.encodePacked(_myString));
+    }
+
+    function getBytesLength() public view returns (uint) {
+        return myBytes.length;
+    }
+}
